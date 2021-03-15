35fb1bd8234111521c597e24ac2e069a19a86d47
icetree158 <79416180+icetree158@users.noreply.github.com>
2021-03-16 00:31:53 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  let openBracket = 0;
+  if (str.length % 2 != 0)
+      return false;
+  for (let i = 0; i < str.length; i++) {
+      for (const [a, b] of bracketsConfig) {
+          if (str[i] == b) {
+              if (stack[openBracket - 1] == a) {
+                  stack.pop();
+                  openBracket -= 1;
+              } else {
+                  stack.push(str[i]);
+                  openBracket++;
+              }
+
+          } else if (str[i] == a) {
+              stack.push(str[i]);
+              openBracket += 1;
+          }
+      }
+  }
+
+  return !stack.length;
 }
